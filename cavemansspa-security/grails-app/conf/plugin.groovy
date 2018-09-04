77afa0531b3962d9032d2f3ec2127a16703aada1
cavemansspa <cavemansspa@gmail.com>
2018-09-04 11:45:48 -0400

    Change stateless access to /api + Cleanup of s2-quickstart default configurations

--- cavemansspa-security/grails-app/conf/plugin.groovy
+++ cavemansspa-security/grails-app/conf/plugin.groovy
@@ -1,5 +1,6 @@
-grails.plugin.springsecurity.rest.token.storage.jwt.secret = 'd6e2ec20-e111-4143-8605-ee29c3dc1189'
+grails.plugin.springsecurity.rest.token.storage.jwt.secret = 'd6e2ec20e11141438605ee29c3dc1189'
 grails.plugin.springsecurity.rest.token.validation.enableAnonymousAccess = true
+grails.plugin.springsecurity.useSecurityEventListener = true
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'io.cavemansspa.security.User'
@@ -10,32 +11,30 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
         [pattern: '/', access: ['permitAll']],
         [pattern: '/error', access: ['permitAll']],
         [pattern: '/index', access: ['permitAll']],
-        [pattern: '/index.gsp', access: ['permitAll']],
-        [pattern: '/shutdown', access: ['permitAll']],
-        [pattern: '/assets/**', access: ['permitAll']],
-        [pattern: '/**/js/**', access: ['permitAll']],
-        [pattern: '/**/css/**', access: ['permitAll']],
-        [pattern: '/**/images/**', access: ['permitAll']],
-        [pattern: '/**/favicon.ico', access: ['permitAll']]
+
+        // Enable Spring Security stateful login endpoints.
+        // Optional, remove if you don't need stateful auth.
+        [pattern: '/login/**', access: ['permitAll']],
+        [pattern: '/logout', access: ['permitAll']],
+        [pattern: '/logout/**', access: ['permitAll']],
+
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
-        [pattern: '/assets/**', filters: 'none'],
-        [pattern: '/**/js/**', filters: 'none'],
-        [pattern: '/**/css/**', filters: 'none'],
-        [pattern: '/**/images/**', filters: 'none'],
-        [pattern: '/**/favicon.ico', filters: 'none'],
 
-        //Stateless chain
+        // Enable access to static assets from asset-pipeline.
+        [pattern: '/assets/**', filters: 'none'],
+
+        //Stateless chain -- we will use /api for SPA api based stateless requests.
         [
-                pattern: '/**',
-//                filters: 'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'
+                pattern: '/api/**',
+                // Let anonymous access through.
                 filters: 'JOINED_FILTERS,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'
         ],
 
         //Traditional, stateful chain
         [
-                pattern: '/stateful/**',
+                pattern: '/**',
                 filters: 'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter'
         ]
 ]
